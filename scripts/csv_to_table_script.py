0e52c600dfe25ddf875aade993bf1978ab7c741c
Joaquin Brm <joaquin.bermudez.a@gmail.com>
2025-07-22 15:48:47 -0300

    Updated to fit generic datasets.

--- scripts/csv_to_table_script.py
+++ scripts/csv_to_table_script.py
@@ -1,5 +1,7 @@
 import psycopg2
 import csv
+import pandas as pd
+import os
 
 #Credenciales de conexión
 DB_HOST = 'localhost'
@@ -20,26 +22,29 @@ def conectar_db():
         port=DB_PORT
     )
 
-path_csv = ''
+def cargar_datos(path_carpeta : str):
+    """Ingesta datos de un csv a una tabla en PostgreSQL
+    
+        Parámetros: path de carpeta con los csv.
+        Requiere: las tablas tienen el mismo nombre que los csv"""
+    for nombre_tabla in path_carpeta: #REVISAR
+        #Conectar db y csv
+        archivo = open(nombre_tabla, 'r')
+        conex = conectar_db()
+        cursor = conex.cursor()
 
-def ingestar_datos():
-    #Conectarse a db
-    archivo_csv = open(path_csv, 'r')
-    conex = conectar_db()
-    cursor = conex.cursor()
+        #Abre y lee el csv
+        data_reader = pd.read_csv(archivo)
+        next(data_reader) #Saltea def de columnas
+        for fila in data_reader:
+            cursor.execute("INSERT INTO"+nombre_tabla+"VALUES (%s, %s, %s)", fila) #REVISAR %s
 
-    #Abre y lee el CSV
-    data_reader = csv.reader(archivo_csv)
-    next(data_reader) #Saltea def de columnas
-    for fila in data_reader:
-        cursor.execute("INSERT INTO ... VALUES (%s, %s, %s)", fila) #REVISAR: crear función para el string de la querie.
-
-    #Commitear y cerrar conexión
-    conex.commit()
-    cursor.close()
-    conex.close()
-    archivo_csv.close()
-    print('Los datos se ingestaron correctamente')
+        #Commitear y cerrar conexión
+        conex.commit()
+        cursor.close()
+        conex.close()
+        archivo.close()
+        print('Se completó la carga de datos para la tabla'+nombre_tabla+'.')
 
 if __name__ == "__main__":
-    ingestar_datos()
+    cargar_datos()
